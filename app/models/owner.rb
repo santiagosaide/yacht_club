5fe773ef5a33bd06cef9b412cecfd93e2cc623c9
Raghu Betina <raghu@firstdraft.com>
2018-06-15 03:01:49 +0000

    Direct association between vessels and owners

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,9 @@
 class Owner < ApplicationRecord
   # Direct associations
 
+  belongs_to :vessels,
+             :class_name => "Vessel"
+
   belongs_to :member
 
   # Indirect associations
--- app/models/vessel.rb
+++ app/models/vessel.rb
@@ -1,6 +1,10 @@
 class Vessel < ApplicationRecord
   # Direct associations
 
+  has_many   :owners,
+             :foreign_key => "vessels_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
