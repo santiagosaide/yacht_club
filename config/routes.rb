ceefba8d87e74cf624c23cd5f8eb7afafa62f7c8
Raghu Betina <raghu@firstdraft.com>
2018-06-15 03:01:31 +0000

    Generate members resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :members
+  # Routes for the Member resource:
+  # READ
+  get "/members", :controller => "members", :action => "index"
+  get "/members/:id", :controller => "members", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
