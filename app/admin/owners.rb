4b23381c5c7ac2f42b5c7ec31415c54a6e5f2103
Raghu Betina <raghu@firstdraft.com>
2018-06-15 03:02:14 +0000

    Whitelist columns in Owner for ActiveAdmin

--- app/admin/owners.rb
+++ app/admin/owners.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Owner do
+
+ permit_params :vessels_id, :member_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
