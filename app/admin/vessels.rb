c5a678a9173c69ef79eb79af915a5eeec24a445e
Raghu Betina <raghu@firstdraft.com>
2018-06-15 03:02:07 +0000

    Whitelist columns in Vessel for ActiveAdmin

--- app/admin/vessels.rb
+++ app/admin/vessels.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Vessel do
+
+ permit_params :name, :lenght, :brand, :construction_date, :model, :city, :country_of_origin, :propulsion
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
