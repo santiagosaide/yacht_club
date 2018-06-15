9424de71830ab05f546c4ab340ea056b1d0f362a
Raghu Betina <raghu@firstdraft.com>
2018-06-15 03:02:18 +0000

    Whitelist columns in Family for ActiveAdmin

--- app/admin/families.rb
+++ app/admin/families.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Family do
+
+ permit_params :last_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
